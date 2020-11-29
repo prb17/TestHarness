0fd71919cd6419d87fad7474d816b65f9b7edefc
Troy Jenkins <43391559+jenki5@users.noreply.github.com>
2020-11-28 19:24:37 -0600

    Add files via upload

---
+++ include/ThreadPool.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <chrono>
+
+
+class ThreadPool
+{
+public:
+
+    ThreadPool(int threads);
+    ~ThreadPool();
+    void doJob(std::function <void(void)> func);
+
+protected:
+
+    void threadEntry(int i);
+
+    std::mutex lock_;
+    std::condition_variable condVar_;
+    bool shutdown_;
+    std::queue <std::function <void(void)>> jobs_;
+    std::vector <std::thread> threads_;
+};
